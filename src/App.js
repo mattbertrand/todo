ddb595b1dcbde979f5abf750b49751c106b712dd
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-02 14:15:39 -0700

    added props and hooks to form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Form from './components/Form'
 import TodoList from './components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
       <h1>Matt's Todo List</h1>
     </header>
-    <Form />
+    <Form setInputText={setInputText}/>
     <TodoList />
     </div>
   );
