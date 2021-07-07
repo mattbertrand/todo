3612ad642d3713f92a304ef39d84f6518a0616e8
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-07 15:11:37 -0700

    handle delete Button passing down props

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Todo from './Todo'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
 
     return (
         <div className="todo-container">
             <ul className="todo-list">
                 {todos.map(todo => (
-                    <Todo text={todo.text} key={todo.id}/>
+                    <Todo setTodos={setTodos} todos={todos} todo={todo} text={todo.text} key={todo.id}/>
                 ))}
             </ul>
         </div>
