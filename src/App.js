f1466394e1f06864035ae259c22e97060efeb07b
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-07 15:54:40 -0700

    handle filter to filter out items by completion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Form from './components/Form'
 import TodoList from './components/TodoList'
 
 function App() {
+  
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilterTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler()
+  }, [todos, status])
+
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilterTodos(todos.filter(todo => todo.completed === true))
+        break
+      case 'uncompleted':
+        setFilterTodos(todos.filter(todo => todo.completed === false))
+        break
+      default:
+        setFilterTodos(todos)
+        break
+    }
+  }
   return (
     <div className="App">
       <header>
@@ -22,6 +42,7 @@ function App() {
     <TodoList 
       todos={todos} 
       setTodos={setTodos}
+      filteredTodos={filteredTodos}
     />
     </div>
   );
