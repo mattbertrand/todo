fd9da7ed1fa75ec912f73e40de3b450a2b8c547c
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-07 15:31:43 -0700

    updated state for todos filter

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import TodoList from './components/TodoList'
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
   return (
     <div className="App">
       <header>
       <h1>Matt's Todo List</h1>
     </header>
-    <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-    <TodoList todos={todos} setTodos={setTodos} />
+    <Form 
+      inputText={inputText}
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      setStatus={setStatus}
+    />
+    <TodoList 
+      todos={todos} 
+      setTodos={setTodos}
+    />
     </div>
   );
 }
